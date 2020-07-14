95f881bcc903c09e9abb2e3760e57ea86e2723c4
Michael Lee <lee.cs.michael@gmail.com>
2020-07-14 11:50:55 -0700

    Defined author and language model and added changes to database

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -13,10 +13,12 @@ class Genre(models.Model):
 
 class Book(models.Model):
 	title=models.CharField(max_length=200)
+	#You must use the name of the model as a string if the associated class has not yet been defined in this file before it is referenced!
 	author=models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 	summary=models.TextField(max_length=1000, help_text='Enter a brief def description of the book')
 	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href=“https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre=models.ManyToManyField(Genre, help_text='Select a genre for this book')
+	language=models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
 
 	def __str__(self):
 		return self.title
@@ -44,4 +46,27 @@ class BookInstance(models.Model):
 
 	def __str__(self):
 		return f'{self.id}({self.book.title})'
-		
+
+class Author(models.Model):
+	first_name=models.CharField(max_length=100)
+	last_name=models.CharField(max_length=100)
+	date_of_birth=models.DateField(null=True, blank=True)
+	date_of_death=models.DateField('Died', null=True, blank=True)
+
+	class Meta:
+		ordering=['last_name', 'first_name']
+
+	def __str__(self):
+		return f'{self.last_name}, {self.first_name}'
+
+class Language(models.Model):
+	name=models.CharField(max_length=200,help_text='Enter the language')
+
+	def __str__(self):
+		return self.name
+
+
+
+
+
+
