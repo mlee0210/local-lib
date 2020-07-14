e18cb592e29a1e353efd37d747edf363f04c2131
Michael Lee <lee.cs.michael@gmail.com>
2020-07-14 15:50:34 -0700

    Updated layout for admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,47 @@ from .models import Book, Author, Genre, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class BooksInline(admin.TabularInline):
+	model = Book
+	extra = 0
+
+# Define the admin class                                   
+class AuthorAdmin(admin.ModelAdmin):
+	list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+	fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+	inlines = [BooksInline]
+	#pass
+# Register the admin class with the associated model       
+admin.site.register(Author, AuthorAdmin)   
+
+class BooksInstanceInline(admin.TabularInline):
+	model = BookInstance  
+	#removes spare book instances that were set by default
+	extra = 0
+
+# Register the Admin classes for Book using the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):                                   
+    list_display = ('title', 'author', 'display_genre')
+    inlines = [BooksInstanceInline]
+	
+class BookInstanceAdmin(admin.ModelAdmin):
+	#list_filter = ('status', 'due_back')
+	list_display = ('book', 'status', 'due_back', 'id')
+	fieldsets = (
+		(None, {
+			'fields': ('book', 'imprint', 'id')
+			}),
+		('Availability', {
+			'fields': ('status', 'due_back')
+			}),
+		)
+    #pass 
+
+admin.site.register(BookInstance, BookInstanceAdmin)
