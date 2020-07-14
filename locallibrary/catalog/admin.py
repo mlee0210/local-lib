548775e04004ecc5bb93e1c4cd376e60a7f7e29a
Michael Lee <lee.cs.michael@gmail.com>
2020-07-14 14:48:45 -0700

    Created superuser and stored data in db

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Book, Author, Genre, BookInstance, Language
 
 # Register your models here.
+
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(BookInstance)
+admin.site.register(Language)
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -15,7 +15,7 @@ class Book(models.Model):
 	title=models.CharField(max_length=200)
 	#You must use the name of the model as a string if the associated class has not yet been defined in this file before it is referenced!
 	author=models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-	summary=models.TextField(max_length=1000, help_text='Enter a brief def description of the book')
+	summary=models.TextField(max_length=1000, help_text='Enter a brief description of the book')
 	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href=“https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 	genre=models.ManyToManyField(Genre, help_text='Select a genre for this book')
 	language=models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
