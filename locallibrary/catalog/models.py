74b4af6238d1e6ef2bdec516471d8f1e6de78d0a
Michael Lee <lee.cs.michael@gmail.com>
2020-07-14 11:10:57 -0700

    Defined genre and book model

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Genre(models.Model):
+	name=models.CharField(max_length=200, help_text='Enter a book genre(e.g. Science Fiction)')
+
+	def __str__(self):
+		return self.name
+
+class Book(models.Model):
+	title=models.CharField(max_length=200)
+	author=models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
+	summary=models.TextField(max_length=1000, help_text='Enter a brief def description of the book')
+	isbn = models.CharField('ISBN', max_length=13, help_text='13 Character <a href=“https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
+	genre=models.ManyToManyField(Genre, help_text='Select a genre for this book')
+
+	def __str__(self):
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('book-detail',args=[str(self.id)])
+
